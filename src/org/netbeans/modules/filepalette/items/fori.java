db8f781cf1051749d9ece77fcd90836c5a5aa115
unknown <wjellesma@qmail.qcc.edu>
2013-05-01 21:53:38 -0400

    Update 5-1-13

---
+++ src/org/netbeans/modules/filepalette/items/fori.java
@@ -0,0 +1,90 @@
+
+/*
+*
+*
+* To change this template, choose Tools | Templates
+* and open the template in the editor.
+ */
+package org.netbeans.modules.filepalette.items;
+
+//~--- non-JDK imports --------------------------------------------------------
+
+import org.netbeans.modules.filepalette.items.resources.foriItemCustomizer;
+
+import org.openide.text.ActiveEditorDrop;
+
+//~--- JDK imports ------------------------------------------------------------
+
+import javax.swing.text.BadLocationException;
+import javax.swing.text.JTextComponent;
+
+/**
+ *
+ *
+ */
+public class fori implements ActiveEditorDrop {
+    private String comment = "";
+
+    /**
+     *
+     */
+    public String abbr = "fori";
+
+    /**
+     *
+     */
+    public fori() {}
+
+    private String createBody() {
+        String        comment = getComment();
+        StringBuilder buffer  = new StringBuilder();
+
+        ////////////////////////
+        buffer.append("// ").append(comment).append("\n");
+        buffer.append(abbr);
+
+        ////////////////////////
+        return buffer.toString();
+    }
+
+    /**
+     *
+     * @param targetComponent
+     * @return
+     */
+    public boolean handleTransfer(JTextComponent targetComponent) {
+        foriItemCustomizer c      = new foriItemCustomizer(this, targetComponent);
+        boolean            accept = c.showDialog();
+
+        if (accept) {
+            String body = this.createBody();
+
+            try {
+                FilePaletteUtilities.insertm(body, targetComponent, true);
+            } catch (BadLocationException ble) {
+                accept = false;
+            }
+        }
+
+        return accept;
+    }
+
+    /**
+     *
+     * @return
+     */
+    public String getComment() {
+        return comment;
+    }
+
+    /**
+     *
+     * @param comment
+     */
+    public void setComment(String comment) {
+        this.comment = comment;
+    }
+}
+
+
+//~ Formatted by Jindent --- http://www.jindent.com
